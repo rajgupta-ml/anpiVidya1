b4069a2fa0a58b72e2d2c0f16b8fabd56b5baa5d
Raj Gupta <pratikrajgupta23@gmail.com>
2023-03-05 22:01:40 +0530

    Developed the Login and Registration with clean architecture

--- Server/persistance/createJwt.js
+++ Server/persistance/createJwt.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
 
-const createJwt = (email) => jwt.sign({ data: email }, process.env.JWT_SECERET, { expiresIn: '1h' });
+const createJwt = (email) => jwt.sign({ data: email }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
 export default createJwt;
