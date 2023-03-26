15328692ddb2e5f21904a0772dfc2fdeb22bbf14
Raj Gupta <pratikrajgupta23@gmail.com>
2023-03-26 22:34:47 +0530

    Developing the Suggestion system

---
+++ Server/persistance/pastClassroomInvitesPersistance.js
@@ -0,0 +1,5 @@
+const pastClassroomInvitesPersistance = () => {
+
+};
+
+export default pastClassroomInvitesPersistance;
---
+++ Server/persistance/rankStudentPersistance.js
@@ -0,0 +1,5 @@
+const rankStudentPersistance = () => {
+
+};
+
+export default rankStudentPersistance;
