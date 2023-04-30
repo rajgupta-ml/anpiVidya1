9476d390019129a06e9bc73ac85b4fa61cac4aff
Anurag <bhootnath@gmail.com>
2023-04-30 23:00:51 +0530

    new notes ui

--- Client/src/components/dashboard-components/notes-components/Notes.jsx
+++ Client/src/components/dashboard-components/notes-components/Notes.jsx
@@ -25,16 +25,30 @@ function Notes() {
         <SideNavigation />
       </div>
       {/* MIDDLE SECTION FOR DESKTOP */}
-      <section className="font-['poppins'] flex flex-col w-[80%] ml-[265px] h-[100vh] uppercase" style={{ width: 'calc(100vw - 280px)' }}>
+      <section className="font-['poppins'] flex flex-col w-[80%] ml-[265px] h-[100vh] " style={{ width: 'calc(100vw - 280px)' }}>
         <div className="h-full ml-[70px] mr-[70px] mt-[50px] flex flex-col gap-6">
           <div className="heading | bg-[#0079BC] text-white font-[poppins] text-[44px] font-bold py-[30px] px-[50px]  rounded-[20px]">AI-POWERED SMART NOTES</div>
-          <div className="bg-[#0079BC] h-full overflow-y-auto rounded-[20px] flex justify-around p-[40px] gap-4">
-            <div className="flex flex-col">
-              <form action="">
-                <input type="text" />
+          <div className="bg-[#0079BC] h-[80vh] overflow-y-auto rounded-[20px] flex flex-col justify-start p-[40px] gap-4">
+            <div className="flex justify-around min-h-[20rem] min-w-[50rem] ">
+              <form onSubmit={(e) => handleSubmit(e, notesHeading)}>
+                <div className="text-[44px] text-white font-bold my-4 text-center">GPT-enabled prompt</div>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setNotesHeading(e.target.value);
+                  }}
+                  className="rounded-[20px] h-[4rem] px-4 py-2 w-full text-[24px]"
+                  id="note"
+                  name="note"
+                  placeholder="POINT TO BE SUMMERIZED"
+                />
+                <button type="submit" className="bg-[#FFC100] text-[28px] px-4 py-3 rounded-[20px] w-full border-[1px] border-black flex justify-center items-center mt-4 drop-shadow-[4px_4px_0px_rgba(0,0,0,1)]">GENERATE</button>
               </form>
+              <div className="min-w-[40rem] max-w-[40rem] text-[24px] text-white ">
+                The OpenAI API is a set of machine learning models and algorithms that are made available for developers to use in their own applications. The API provides access to state-of-the-art natural language processing (NLP) models, including text generation, summarization, question-answering, sentiment analysis, and language translation.
+              </div>
             </div>
-            <div>OUTPUT</div>
+            <div className=" text-[28px] text-white " id="result" />
           </div>
         </div>
       </section>
